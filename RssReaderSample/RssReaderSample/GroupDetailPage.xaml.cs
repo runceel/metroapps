d7d3a3e1df638cc8e7489fdc619e081e69402370
Kazuki <k.ota.0130@gmail.com>
2012-08-23 18:33:22 +0900

    RSSReaderSample RTM start.

---
+++ RssReaderSample/RssReaderSample/GroupDetailPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using RssReaderSample.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// グループ詳細ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234229 を参照してください
+
+namespace RssReaderSample
+{
+    /// <summary>
+    /// 単一のグループ内のアイテムのプレビューを含め、グループの概要を表示する
+    /// ページです。
+    /// </summary>
+    public sealed partial class GroupDetailPage : RssReaderSample.Common.LayoutAwarePage
+    {
+        public GroupDetailPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// このページには、移動中に渡されるコンテンツを設定します。前のセッションからページを
+        /// 再作成する場合は、保存状態も指定されます。
+        /// </summary>
+        /// <param name="navigationParameter">このページが最初に要求されたときに
+        /// <see cref="Frame.Navigate(Type, Object)"/> に渡されたパラメーター値。
+        /// </param>
+        /// <param name="pageState">前のセッションでこのページによって保存された状態の
+        /// ディクショナリ。ページに初めてアクセスするとき、状態は null になります。</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // TODO: 問題のドメインでサンプル データを置き換えるのに適したデータ モデルを作成します
+            var group = SampleDataSource.GetGroup((String)navigationParameter);
+            this.DefaultViewModel["Group"] = group;
+            this.DefaultViewModel["Items"] = group.Items;
+        }
+
+        /// <summary>
+        /// アイテムがクリックされたときに呼び出されます。
+        /// </summary>
+        /// <param name="sender">クリックされたアイテムを表示する GridView (アプリケーションがスナップ
+        /// されている場合は ListView) です。</param>
+        /// <param name="e">クリックされたアイテムを説明するイベント データ。</param>
+        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // 適切な移動先のページに移動し、新しいページを構成します。
+            // このとき、必要な情報をナビゲーション パラメーターとして渡します
+            var itemId = ((SampleDataItem)e.ClickedItem).UniqueId;
+            this.Frame.Navigate(typeof(ItemDetailPage), itemId);
+        }
+    }
+}
---
+++ RssReaderSample/RssReaderSample/ItemDetailPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using RssReaderSample.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// アイテム詳細ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234232 を参照してください
+
+namespace RssReaderSample
+{
+    /// <summary>
+    /// グループ内の単一のアイテムに関する詳細情報を表示するページです。同じグループに属する他の
+    /// アイテムにフリップするジェスチャを使用できます。
+    /// </summary>
+    public sealed partial class ItemDetailPage : RssReaderSample.Common.LayoutAwarePage
+    {
+        public ItemDetailPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// このページには、移動中に渡されるコンテンツを設定します。前のセッションからページを
+        /// 再作成する場合は、保存状態も指定されます。
+        /// </summary>
+        /// <param name="navigationParameter">このページが最初に要求されたときに
+        /// <see cref="Frame.Navigate(Type, Object)"/> に渡されたパラメーター値。
+        /// </param>
+        /// <param name="pageState">前のセッションでこのページによって保存された状態の
+        /// ディクショナリ。ページに初めてアクセスするとき、状態は null になります。</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // 保存されたページの状態で、表示する最初のアイテムをオーバーライドすることを許可します
+            if (pageState != null && pageState.ContainsKey("SelectedItem"))
+            {
+                navigationParameter = pageState["SelectedItem"];
+            }
+
+            // TODO: 問題のドメインでサンプル データを置き換えるのに適したデータ モデルを作成します
+            var item = SampleDataSource.GetItem((String)navigationParameter);
+            this.DefaultViewModel["Group"] = item.Group;
+            this.DefaultViewModel["Items"] = item.Group.Items;
+            this.flipView.SelectedItem = item;
+        }
+
+        /// <summary>
+        /// アプリケーションが中断される場合、またはページがナビゲーション キャッシュから破棄される場合、
+        /// このページに関連付けられた状態を保存します。値は、
+        /// <see cref="SuspensionManager.SessionState"/> のシリアル化の要件に準拠する必要があります。
+        /// </summary>
+        /// <param name="pageState">シリアル化可能な状態で作成される空のディクショナリ。</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+            var selectedItem = (SampleDataItem)this.flipView.SelectedItem;
+            pageState["SelectedItem"] = selectedItem.UniqueId;
+        }
+    }
+}
